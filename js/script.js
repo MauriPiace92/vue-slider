a123e0660162ecdedaa5ffe0d163ef21c5af0f21
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-04-20 16:59:24 +0200

    inizio parte (@click)

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@ var app= new Vue(
     {
         el:'#root',
         data:{
+            countIndexImg: 3,
+            images:[
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp'
+            ],
+            visible: true
+
             
             
         },
         methods:{
+            nextImage(){
+                
+            }
 
         }
     }
